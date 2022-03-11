cbdec7c019d4ab72b11139c825fa258e39f0ca78
johnny <xjohn117@outlook.com>
2022-03-11 00:19:35 -0500

    end point listos

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+var port = { dev: 3100, prod: 3000 }
+
 
 const mariadb = require('mariadb');
 const pool = mariadb.createPool({
@@ -9,15 +11,18 @@ const pool = mariadb.createPool({
     connectionLimit: 5
 });
 
+app.get('/saludo/:key1/:key2', function(req, res) {
+    const { valor1, valor2 } = req.query //con query se recibe datos por parametro de get
+    res.send('<h1>hola ' + valor1 + ' - ' + valor2 + '</h1>')
+})
 
-app.get('/saludo', function(req, res) {
+app.get('/getUsers', function(req, res) {
     pool.getConnection()
         .then(conn => {
-
             conn.query("SELECT * FROM trabajofinaldb.usuarios")
                 .then((rows) => {
                     console.log(rows);
-                    return res.status(200).json({ saludo: rows })
+                    return res.status(200).json(rows)
                 })
                 .then((res) => {
                     console.log(res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
@@ -35,6 +40,61 @@ app.get('/saludo', function(req, res) {
 
 });
 
+app.get('/getUser/:email?', function(req, res) {
+    console.log("==========>1", req.query.email)
+    pool.getConnection()
+        .then(conn => {
+            conn.query("SELECT * FROM trabajofinaldb.usuarios where email = ?", [req.query.email])
+                .then((rows) => {
+                    console.log(rows);
+                    return res.status(200).json(rows)
+                })
+                .then((res) => {
+                    console.log(res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
+                    conn.end();
+                })
+                .catch(err => {
+                    //handle error
+                    console.log(err);
+                    conn.end();
+                })
+
+        }).catch(err => {
+            //not connected
+        });
+
+});
+
+app.get('/setUser', function(req, res) {
+    pool.getConnection()
+        .then(conn => {
+            conn.query(`
+            INSERT INTO 
+            trabajofinaldb.usuarios (
+                nombre,
+                ciudad,
+                telefono,
+                email) 
+                VALUES
+                (?, ?, ?, ?);`, ['name', 'city', 'phone', 'email'])
+                .then((rows) => {
+                    console.log("response=>", rows);
+                    return res.status(200).json({ response: 'ok' })
+                })
+                .then((res) => {
+                    console.log(res);
+                    conn.end();
+                })
+                .catch(err => {
+                    //handle error
+                    console.log(err);
+                    conn.end();
+                })
+
+        }).catch(err => {
+            //not connected
+        });
+})
 
 
-app.listen(3000, () => { console.log('listening on port 3000'); });
+app.listen(port.prod, () => { console.log('listening on port 3000'); });
